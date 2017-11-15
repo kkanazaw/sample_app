155f6491e55dd0d08a766c5fc91458367a143063
kkanazaw <kkanazaw@designrule.jp>
2017-11-15 23:11:42 +0900

    Add user microposts
    画像のアップロードは飛ばした。
    ImageMagicいれるのが面倒だったので

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,4 +8,14 @@ class ApplicationController < ActionController::Base
   def hello
     render text: "hello, world!"
   end
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  
 end
